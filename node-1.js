9bbd44936606624a29153ccb047a9803f836e4d2
wenjie_yuan <m15517530152@163.com>
2020-09-05 09:59:21 +0800

    node第一天学习

---
+++ node-1.js
@@ -0,0 +1,11 @@
+var http = require('http')  //引入http模块
+// 创建一个http服务器 并设置回调函数
+http.createServer(function(request, response){
+    // 当访问8124接口的时候就会执行这个回调函数
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    // response.end 返回的数据
+    response.end('Hellow Word')
+}).listen(8124)  //设置端口号
+// 在node环境 输出的日志
+console.log('Server running at http://127.0.0.1:8124/')
+
